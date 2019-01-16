89c4a03629a0ab4a573d40beed8baf7aee0102e9
Sherry Jain <sherryjain17@gmail.com>
2019-01-17 03:19:45 +0530

    made grid function and added cdn

--- Design.js
+++ Design.js
@@ -1,3 +1,16 @@
-var h,w;
-h=$('#gridh').val();
-w=$('#gridw').val();
+var row,cell;
+$('#sub').on('click',function(){
+    row=$('#gridh').val();
+    cell=$('#gridw').val();
+    $('tr').remove();
+    makeGrid(row,cell);
+});
+function makeGrid(r,c){
+for(var i=0;i<r;i=i+1)
+{   $("#grid").append("<tr></tr>");
+    for(var j=0;j<c;j=j+1)
+    {   
+        $("tr:last").append("<td></td>");
+    }
+}
+}
