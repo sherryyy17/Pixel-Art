b99e0c7c62d04b0ddeff0be6b8085dbf0156848d
Sherry Jain <sherryjain17@gmail.com>
2019-01-22 02:13:05 +0530

    added comments and removed prevemtDelay

--- Design.js
+++ Design.js
@@ -1,29 +1,34 @@
+//make grid when form is submitted
 $("#para").submit(function(click)
-{   event.preventDefault;
+{  
     var row=$("#gridh").val();
     var colm=$("#gridw").val();
-    $("tr").remove();
+    $("tr").remove(); //deinitialises grid
     $("button").remove();
     for(var i=0;i<row;i++){
         $("#grid").append("<tr></tr>");
     for(var j=0;j<colm;j++){
             $("tr:last").append("<td></td>");
-        }
-        event.preventDefault;   
+            //adds cells to the the last child tr
+        }   
     }
     $("body").append("<button type='reset'>reset</button>");
-
+    //reset button
 });
+
+//to fill the clicked cells with selected colors 
 $("#grid").on("click","td",function(){
     if($(this).attr("style"))
-        $(this).removeAttr("style");
+        $(this).removeAttr("style"); //unfill filled cells
     else
         $(this).css("background",function(){
         var colo=$("#color").val();
         return colo;
+        //callback func
     });
 }
 );
+//reset the grid
 $("body").on("click","button",function(){
     $("td").removeAttr("style");
 });
